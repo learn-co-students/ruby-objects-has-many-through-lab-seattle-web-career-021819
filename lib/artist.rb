a2f5162b28f254946aeae065cb1f350dfcdfd90e
Chhaian Pin <chhaianpin@hotmail.com>
2019-02-23 22:32:40 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+class Artist
+  
+  attr_accessor :name, :songs
+  
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+  
+  def genres
+    self.songs.map do |song|
+      song.genre
+    end
+  end
+  
+end
---
+++ lib/genre.rb
@@ -0,0 +1,24 @@
+class Genre
+  @@all = []
+  
+  attr_accessor :artists
+  attr_reader :name, :songs
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+    @artists = []
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def genres
+    Songs.all.map do |song|
+      song.artist == self
+    end.uniq
+  end
+  
+end
